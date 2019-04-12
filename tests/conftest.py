9de68e20da2d51f14d1747b7d5c2fe7cc26c3a0d
Lê Hải Nam <nam.lh@teko.vn>
2019-04-12 17:10:51 +0700

    Move test into test suit

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,55 +1,6 @@
-import os
-import pytest
-import requests
-
-
 def pytest_addoption(parser):
     parser.addoption(
         '--submit-tests',
         action='store_true',
         help='Submit tests to Jira'
     )
-
-
-@pytest.hookimpl(hookwrapper=True)
-def pytest_pyfunc_call(pyfuncitem):
-    outcome = yield
-
-    should_submit_tests = pyfuncitem.config.getoption('--submit-tests')
-
-    if should_submit_tests is False:
-        return
-
-    JIRA_PROJECT_KEY = os.environ.get('JIRA_PROJECT_KEY')
-
-    for marker in pyfuncitem.own_markers:
-        if marker.name.startswith(JIRA_PROJECT_KEY):
-            test_split_name = marker.name.split('_')
-            if len(test_split_name) < len(['PROJECTKEY', 'TESTKEY']):
-                continue
-
-            JIRA_URL = os.environ.get('JIRA_URL')
-            JIRA_USERNAME = os.environ.get('JIRA_USERNAME')
-            JIRA_PASSWORD = os.environ.get('JIRA_PASSWORD')
-            if not JIRA_URL or \
-               not JIRA_PROJECT_KEY or \
-               not JIRA_USERNAME or not JIRA_PASSWORD:
-                continue
-
-            TEST_CASE_KEY = JIRA_PROJECT_KEY + '-' + test_split_name[1]
-            data = {
-                'projectKey': JIRA_PROJECT_KEY,
-                'testCaseKey': TEST_CASE_KEY
-            }
-
-            try:
-                outcome.get_result()
-                data['status'] = 'Pass'
-            except Exception:
-                data['status'] = 'Fail'
-            finally:
-                requests.post(
-                    JIRA_URL + '/testresult',
-                    auth=(JIRA_USERNAME, JIRA_PASSWORD),
-                    json=data
-                )
--- tests/test_plus.py
+++ tests/test_plus.py
@@ -1,14 +1,15 @@
 import pytest
-from app import simple_plus
+from calculator import simple_plus
 
 
-@pytest.mark.IAM_30
-def test_simple_plus():
-    assert simple_plus(1, 2) == 3
-    assert simple_plus(27, 4) == 31
+class TestSimplePlus:
+    def __init__(self):
+        self.issue_key = 'IAM-20'
 
+    def should_calculate_plus_correctly():
+        assert simple_plus(1, 2) == 3
+        assert simple_plus(27, 4) == 31
 
-@pytest.mark.IAM_31
-def test_simple_plus_using_string():
-    with pytest.raises(TypeError):
-        simple_plus('string', 'string')
+    def should_throw_error_when_input_strings():
+        with pytest.raises(TypeError):
+            simple_plus('string', 'string')
