94def045fd111a3942c300fbea9da1aea41cd6c3
Lê Hải Nam <nam.lh@teko.vn>
2019-04-16 10:16:58 +0700

    Able to submit test cycle with test case in it

--- tests/utils.py
+++ tests/utils.py
@@ -15,7 +15,7 @@ class JiraTestService():
         self.project_key = jira_settings['project_key']
         self.auth_string = (jira_settings['user'],
                             jira_settings['password'])
-        self.url = jira_settings['url'] + '/rest/atm/1.0/testcase'
+        self.url = jira_settings['url'] + '/rest/atm/1.0'
 
     def get_tests_in_issue(self, issue_key):
         params = {
@@ -23,19 +23,19 @@ class JiraTestService():
             'projectKey = "%s" AND issueKeys IN (%s)' %
             (self.project_key, issue_key)
         }
-        response = requests.get(url=self.url + 'search',
+        response = requests.get(url=self.url + '/testcase/search',
                                 params=params,
                                 auth=self.auth_string).json()
-        return list(map(itemgetter('key', response)))
+        return list(map(itemgetter('key'), response))
 
     def create_test(self, test_name, issue_key):
         json = {
             'name': test_name,
-            'projectKey': self.jira_settings['projectKey'],
+            'projectKey': self.project_key,
             'issueLinks': [issue_key],
             'status': 'Approved'
         }
-        response = requests.post(url=self.url,
+        response = requests.post(url=self.url + '/testcase',
                                  json=json,
                                  auth=self.auth_string)
         if response.status_code != 201:
@@ -46,29 +46,51 @@ class JiraTestService():
         return test_key
 
     def delete_test(self, test_key):
-        response = requests.delete(url=self.url + test_key,
+        response = requests.delete(url=self.url + '/testcase/' + test_key,
                                    auth=self.auth_string)
         if response.status_code != 204:
             raise Exception('Delete test return with error status code',
                             response.status_code)
 
-    def create_test_cycle(self):
-        pass
+    def create_test_cycle(self, name, issue_key, items):
+        json = {
+            'name': name,
+            'projectKey': self.project_key,
+            'issueKey': issue_key,
+            'items': items
+        }
+        response = requests.post(url=self.url + '/testrun',
+                                 json=json,
+                                 auth=self.auth_string)
+        if response.status_code != 201:
+            raise Exception('Create test cycle return with error status code',
+                            response.status_code)
 
 
 class JiraTest():
+    test_cycle_items = []
     test_services = JiraTestService(jira_settings)
 
     @classmethod
     def setup_class(cls):
-        print('Setup class', cls)
+        test_keys_list = cls.test_services.get_tests_in_issue(cls.issue_key)
+        for test_key in test_keys_list:
+            cls.test_services.delete_test(test_key)
 
     @classmethod
     def teardown_class(cls):
-        print('Teardown class', cls)
+        cls.test_services.create_test_cycle(cls.issue_key + ' Cycle',
+                                            cls.issue_key,
+                                            cls.test_cycle_items)
 
-    def setup_method(self, test_method):
-        print('Setup method', test_method)
+    def setup_method(self, method):
+        self.test_key = self.test_services.create_test(method.__name__,
+                                                       self.issue_key)
 
-    def teardown_method(self, test_method):
-        print('Teardown method', test_method)
+    def teardown_method(self, method):
+        test_result = 'Pass'
+        json = {
+            'testCaseKey': self.test_key,
+            'status': test_result
+        }
+        self.test_cycle_items.append(json)
