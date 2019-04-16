62bf48b603eada6e7ea141654b513e56d49c0eb8
Lê Hải Nam <nam.lh@teko.vn>
2019-04-16 09:15:22 +0700

    Setup jira test service

--- tests/test_plus.py
+++ tests/test_plus.py
@@ -1,33 +1,10 @@
 import pytest
-from . import JiraTestService
+from .utils import JiraTest
 from calculator import simple_plus
 
 
-jira_settings = {
-    'url': 'https://jira.teko.vn',
-    'user': 'lan.nt',
-    'password': 'QC12345678',
-    'project_key': 'IAM'
-}
-
-
-class TestSimplePlus():
+class TestSimplePlus(JiraTest):
     issue_key = 'IAM-20'
-    test_services = JiraTestService(jira_settings)
-
-    @classmethod
-    def setup_class(cls):
-        print('Setup class', cls)
-
-    @classmethod
-    def teardown_class(cls):
-        print('Teardown class', cls)
-
-    def setup_method(self, test_method):
-        print('Setup method', test_method.name)
-
-    def teardown_method(self, test_method):
-        print('Teardown method', test_method)
 
     def test_should_calculate_plus_correctly(self):
         assert simple_plus(1, 2) == 3
--- tests/utils.py
+++ tests/utils.py
@@ -1,17 +1,34 @@
+from operator import itemgetter
 import requests
 
 
-class Jira_Test_Service:
+jira_settings = {
+    'url': 'https://jira.teko.vn',
+    'user': 'lan.nt',
+    'password': 'QC12345678',
+    'project_key': 'IAM'
+}
+
+
+class JiraTestService():
     def __init__(self, jira_settings):
-        self.jira_settings = jira_settings
-        self.auth_string = (self.jira_settings['user'],
-                            self.jira_settings['password'])
-        self.url = self.jira_settings['url'] + '/rest/atm/1.0/testcase'
+        self.project_key = jira_settings['project_key']
+        self.auth_string = (jira_settings['user'],
+                            jira_settings['password'])
+        self.url = jira_settings['url'] + '/rest/atm/1.0/testcase'
 
     def get_tests_in_issue(self, issue_key):
-        pass
+        params = {
+            'query':
+            'projectKey = "%s" AND issueKeys IN (%s)' %
+            (self.project_key, issue_key)
+        }
+        response = requests.get(url=self.url + 'search',
+                                params=params,
+                                auth=self.auth_string).json()
+        return list(map(itemgetter('key', response)))
 
-    def create_tests(self, test_name, issue_key):
+    def create_test(self, test_name, issue_key):
         json = {
             'name': test_name,
             'projectKey': self.jira_settings['projectKey'],
@@ -29,7 +46,29 @@ class Jira_Test_Service:
         return test_key
 
     def delete_test(self, test_key):
-        pass
+        response = requests.delete(url=self.url + test_key,
+                                   auth=self.auth_string)
+        if response.status_code != 204:
+            raise Exception('Delete test return with error status code',
+                            response.status_code)
 
     def create_test_cycle(self):
         pass
+
+
+class JiraTest():
+    test_services = JiraTestService(jira_settings)
+
+    @classmethod
+    def setup_class(cls):
+        print('Setup class', cls)
+
+    @classmethod
+    def teardown_class(cls):
+        print('Teardown class', cls)
+
+    def setup_method(self, test_method):
+        print('Setup method', test_method)
+
+    def teardown_method(self, test_method):
+        print('Teardown method', test_method)
